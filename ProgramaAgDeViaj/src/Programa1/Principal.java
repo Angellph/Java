4e9f5145c87b1c13fd8af180b800c78bca2d3952
Angellph <82544189+Angellph@users.noreply.github.com>
2022-01-10 18:06:21 -0600

    Add files via upload

---
+++ ProgramaAgDeViaj/src/Programa1/Principal.java
@@ -0,0 +1,24 @@
+package Programa1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author thania
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana ven = new Ventana ();
+        ven.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
