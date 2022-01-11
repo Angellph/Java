05964122081bcc059ffd2e016cf2e45efb101558
Angellph <82544189+Angellph@users.noreply.github.com>
2022-01-10 18:03:40 -0600

    Add files via upload

---
+++ OperacionesGraficas/src/Operaciones/Principal.java
@@ -0,0 +1,16 @@
+package Operaciones;
+
+import java.util.Locale;
+
+
+public class Principal {
+
+    
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+        ventana.setResizable(false);
+    }
+    
+}
